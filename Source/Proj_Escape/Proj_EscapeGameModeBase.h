bf8e2bcc06b50ba4c87118fe3bf6f7c150681e31
HALFMAI-SURFACE\HalfL <halfloligo.h@gmail.com>
2017-09-12 13:07:48 +0800

    添加项目文件。

---
+++ Source/Proj_Escape/Proj_EscapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Proj_EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJ_ESCAPE_API AProj_EscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
